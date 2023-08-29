1d7632b173fa1080a010e22f3b5a8d09045e954b
RoeiLevy <roeilevy99@gmail.com>
2023-08-29 13:09:30 +0300

    fixes for prod

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
-const base_url = 'http://localhost:8080'
+
+let base_url
+if (process.env.NODE_ENV === 'production') {
+    base_url = 'https://type-racer-sandy.vercel.app/api'
+} else {
+    base_url = 'http://localhost:8080/api'
+}
+
 export const getScoresByUserId = async (userId) => {
     const { data } = await axios.get(base_url + `/scores?userId=${userId}`)
     return data
